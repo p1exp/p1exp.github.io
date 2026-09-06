5e06878ab59832145583255b38db21151aec5424
P1EXP <p1exp303@gmail.com>
2026-09-06 15:01:52 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -27,7 +27,7 @@ function loadPageView(pageKey) {
 
     let layoutHtml = '';
 
-    // 1. RENDER MAIN HERO BANNER & HTML TEXT BLOCK
+// 1. RENDER MAIN HERO BANNER
     layoutHtml += `
         <div class="full-banner banner-70">
             <img src="./img_bg/${pageMap.titleBanner.imageName || ''}" class="banner-img">
@@ -35,12 +35,9 @@ function loadPageView(pageKey) {
                 <h1>${pageMap.titleBanner.title || ''}</h1>
             </div>
         </div>
-        <div class="narrow-text-block">
-            ${pageMap.titleBanner.text || ''}
-        </div>
     `;
 
-    // 2. INJECT SOCIAL MATRIX
+    // 2. INJECT SOCIAL MATRIX (Now positioned immediately after the banner)
     if (pageKey === siteConfiguration.navigationMenu[0].id) {
         layoutHtml += `
             <div class="social-matrix-wrapper">
@@ -49,7 +46,7 @@ function loadPageView(pageKey) {
                         <svg viewBox="0 0 448 512"><path d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z"/></svg>
                     </a>
                     <a href="https://www.instagram.com/p1exp" target="_blank" class="social-icon" title="Instagram">
-                        <svg viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+                        <svg viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
                     </a>
                     <a href="https://www.facebook.com/p1exp" target="_blank" class="social-icon" title="Facebook">
                         <svg viewBox="0 0 320 512"><path d="M279.1 288l14.2-92.7h-88.7V135.3c0-25.4 12.4-50.1 52.2-50.1h40.4V11.5S260.6 0 225.4 0C148.6 0 98 46.8 98 130.6V195.3H14v92.7h84v224h100.5v-224h80.6z"/></svg>
@@ -62,7 +59,14 @@ function loadPageView(pageKey) {
         `;
     }
 
-    // 3. RENDER ALL SUB-SECTIONS (BANNERS, HTML TEXT, GRID)
+    // 3. RENDER THE BANNER TEXT BLOCK
+    layoutHtml += `
+        <div class="narrow-text-block">
+            ${pageMap.titleBanner.text || ''}
+        </div>
+    `;
+
+    // 4. RENDER ALL SUB-SECTIONS (BANNERS, HTML TEXT, GRID)
     if (pageMap.sections && pageMap.sections.length > 0) {
         pageMap.sections.forEach(section => {
             
